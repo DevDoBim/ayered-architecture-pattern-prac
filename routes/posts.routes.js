3cd02adc21a2a61b9535660dd463b2bde562e6cf
DevDoBim <zmdkdna2@naver.com>
2023-07-06 21:04:07 +0900

    controller

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const PostsController = require('../controllers/posts.controller');
+const postsController = new PostsController(); // 모듈에 대한 클래스 선언
+
+// API 동작, router와 controller 연결
+router.get('/', postsController.getPosts);
+router.post('/', postsController.createPost);
+
 module.exports = router;
